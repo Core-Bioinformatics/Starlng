9ccdbdd4b1b0a155bc9448244f4f23a1ff02b121
AndiMunteanu <munteanu_andi98@yahoo.com>
2024-07-09 13:41:36 +0300

    [test] Add tests covering perfect / imbalanced chunking of the indices in the nn index function. Add tests for the clustering pipeline.

--- tests/testthat/test-gene_clustering.R
+++ tests/testthat/test-gene_clustering.R
@@ -1,14 +1,65 @@
-test_that("nn2 parallel produces same output", {
-    set.seed(2024)
+test_that("nn2 parallel produces same output - perfect chunks", {
+    set.seed(1234)
     emb <- matrix(runif(1e3 * 30), nrow = 1e3, ncol = 30)
-    k <- 30
+    rownames(emb) <- paste0("cell", seq_len(1e3))
+    k <- 25
 
     default_nn <- parallel_nn2_idx(emb, k)
 
-    doParallel::registerDoParallel(cores = 2)
+    cl <- parallel::makeCluster(2)
+    doParallel::registerDoParallel(cl)
     parallel_nn <- parallel_nn2_idx(emb, k)
+    parallel::stopCluster(cl)
     foreach::registerDoSEQ()
 
     expect_identical(default_nn, parallel_nn)
 })
 
+test_that("nn2 parallel produces same output - imbalanced chunks", {
+    set.seed(1234)
+    emb <- matrix(runif(1e3 * 30), nrow = 1e3, ncol = 30)
+    rownames(emb) <- paste0("cell", seq_len(1e3))
+    k <- 25
+
+    default_nn <- parallel_nn2_idx(emb, k)
+
+    cl <- parallel::makeCluster(3)
+    doParallel::registerDoParallel(cl)
+    parallel_nn <- parallel_nn2_idx(emb, k)
+    parallel::stopCluster(cl)
+    foreach::registerDoSEQ()
+
+    expect_identical(default_nn, parallel_nn)
+})
+
+test_that("clustering pipeline is robust to parallelization - perfect chunks", {
+    set.seed(1234)
+    emb <- matrix(runif(1e3 * 30), nrow = 1e3, ncol = 30)
+    rownames(emb) <- paste0("cell", seq_len(1e3))
+    
+    one_core_cl_output <- clustering_pipeline(emb, n_neighbours = c(5, 50), resolutions = c(0.1, 1))
+
+    cl <- parallel::makeCluster(2)
+    doParallel::registerDoParallel(cl)
+    parallel_cl_output <- clustering_pipeline(emb, n_neighbours = c(5, 50), resolutions = c(0.1, 1))
+    parallel::stopCluster(cl)
+    foreach::registerDoSEQ()
+
+    expect_identical(one_core_cl_output, parallel_cl_output)
+})
+
+test_that("clustering pipeline is robust to parallelization - imbalanced chunks", {
+    set.seed(1234)
+    emb <- matrix(runif(1e3 * 30), nrow = 1e3, ncol = 30)
+    rownames(emb) <- paste0("cell", seq_len(1e3))
+    
+    one_core_cl_output <- clustering_pipeline(emb, n_neighbours = c(5, 50), resolutions = c(0.1, 1))
+
+    cl <- parallel::makeCluster(3)
+    doParallel::registerDoParallel(cl)
+    parallel_cl_output <- clustering_pipeline(emb, n_neighbours = c(5, 50), resolutions = c(0.1, 1))
+    parallel::stopCluster(cl)
+    foreach::registerDoSEQ()
+
+    expect_identical(one_core_cl_output, parallel_cl_output)
+})
