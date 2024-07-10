3b80ee42eaadfd539ab1a60bd6013b7261281f2c
AndiMunteanu <munteanu_andi98@yahoo.com>
2024-07-10 21:16:39 +0300

    [tests] Deprecate the use of 3 cores. Modify the number of points such that the numbers won't divide perfectly.

--- tests/testthat/test-gene_clustering.R
+++ tests/testthat/test-gene_clustering.R
@@ -17,13 +17,13 @@ test_that("nn2 parallel produces same output - perfect chunks", {
 
 test_that("nn2 parallel produces same output - imbalanced chunks", {
     set.seed(1234)
-    emb <- matrix(runif(1e3 * 30), nrow = 1e3, ncol = 30)
-    rownames(emb) <- paste0("cell", seq_len(1e3))
+    emb <- matrix(runif(1001 * 30), nrow = 1001, ncol = 30)
+    rownames(emb) <- paste0("cell", seq_len(1001))
     k <- 25
 
     default_nn <- parallel_nn2_idx(emb, k)
 
-    cl <- parallel::makeCluster(3)
+    cl <- parallel::makeCluster(2)
     doParallel::registerDoParallel(cl)
     parallel_nn <- parallel_nn2_idx(emb, k)
     parallel::stopCluster(cl)
@@ -50,12 +50,12 @@ test_that("clustering pipeline is robust to parallelization - perfect chunks", {
 
 test_that("clustering pipeline is robust to parallelization - imbalanced chunks", {
     set.seed(1234)
-    emb <- matrix(runif(1e3 * 30), nrow = 1e3, ncol = 30)
-    rownames(emb) <- paste0("cell", seq_len(1e3))
+    emb <- matrix(runif(1001 * 30), nrow = 1001, ncol = 30)
+    rownames(emb) <- paste0("cell", seq_len(1001))
     
     one_core_cl_output <- clustering_pipeline(emb, n_neighbours = c(5, 50), resolutions = c(0.1, 1))
 
-    cl <- parallel::makeCluster(3)
+    cl <- parallel::makeCluster(2)
     doParallel::registerDoParallel(cl)
     parallel_cl_output <- clustering_pipeline(emb, n_neighbours = c(5, 50), resolutions = c(0.1, 1))
     parallel::stopCluster(cl)
@@ -63,3 +63,6 @@ test_that("clustering pipeline is robust to parallelization - imbalanced chunks"
 
     expect_identical(one_core_cl_output, parallel_cl_output)
 })
+
+# TODO add tests for `group_by_clusters_general`
+# TODO add tests for `assess_stability_by_clusters`
