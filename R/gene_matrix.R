c8a849258008d6d994de4006c403ada6ed6f81af
AndiMunteanu <munteanu_andi98@yahoo.com>
2024-07-17 00:10:33 +0300

    [feat] Add initial functions that could process the expression matrix to an input for the gene clustering pipeline.

---
+++ R/gene_matrix.R
@@ -0,0 +1,38 @@
+
+# assumes that the expr matrix is gene x cell
+# assumes that the genes have been already filtered i.e only the genes that are meant to be used are present
+get_feature_loading <- function(expr_matrix, npcs = 30) {
+    prcomp(
+        t(expr_matrix),
+        center = TRUE,
+        scale = TRUE,
+        rank. = npcs,
+        retx = TRUE
+    )$rotation
+}
+
+pca_reduction <- function(expr_matrix, npcs = 30) {
+    prcomp(
+        expr_matrix,
+        center = TRUE,
+        scale = TRUE,
+        rank. = npcs
+    )$x
+}
+
+pseudobulk_reduction <- function(expr_matrix, cell_clusters) {
+    if (is.null(colnames(expr_matrix))) {
+        colnames(expr_matrix) <- seq_len(ncol(expr_matrix))
+    }
+
+    barcode_grouping <- split(colnames(expr_matrix), cell_clusters)
+
+    pseudobulk_matrix <- lapply(
+        barcode_grouping,
+        function(barcode_group) {
+            rowMeans(expr_matrix[, barcode_group])
+        }
+    )
+
+    do.call(cbind, pseudobulk_matrix)
+}
