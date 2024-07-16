61776782375d8f2d107221dcf2ea19b408616571
AndiMunteanu <munteanu_andi98@yahoo.com>
2024-07-17 00:09:01 +0300

    [chore] Check if clusters and partitions slots are present in the monocle object.

--- R/pseudotime.R
+++ R/pseudotime.R
@@ -1,83 +1,3 @@
-#' @importFrom dplyr %>% 
-#' @importFrom rlang .data
- 
-select_cells_by_gene_expr <- function(expression_matrix,
-                                      genes,
-                                      thresh_percentile = 0.25,
-                                      thresh_value = 0,
-                                      n_coexpressed_thresh = length(genes)) {
-    genes <- intersect(genes, rownames(expression_matrix))
-
-    if (length(genes) == 0) {
-        stop("No genes found in the expression matrix")
-    }
-
-    expression_matrix <- expression_matrix[genes, ]
-    n_coexpressed <- max(min(n_coexpressed, nrow(expression_matrix)), 1)
-
-    if (thresh_percentile > 0) {
-        thresh_value <- sapply(genes, function(gene) {
-            quantile(sum(expression_matrix[gene, ]), probs = thresh_percentile)
-        })
-    } else {
-        thresh_value <- rep(thresh_value[1], length(genes))
-    }
-
-    mask_expressed <- expression_matrix > thresh_value
-
-    index_cells <- which(colSums(mask_expressed) >= n_coexpressed_thresh)
-
-    if (is.null(colnames(expression_matrix))) {
-        return(index_cells)
-    }
-
-    return(colnames(expression_matrix)[index_cells])
-}
-
-select_cells_by_metadata <- function(metadata_df, metadata_combinations) {
-    mtd_names <- intersect(names(metadata_combinations), colnames(metadata_df))
-
-    if (length(mtd_names) == 0) {
-        stop("No metadata columns found in the metadata dataframe")
-    }
-
-    if (is.null(rownames(metadata_df))) {
-        rownames(metadata_df) <- seq_len(nrow(metadata_df))
-    }
-    
-
-    for (mtd_name in mtd_names) {
-        if (!(all(metadata_combinations[[mtd_name]] %in% unique(metadata_df[[mtd_name]])))) {
-            next
-        }
-
-        metadata_df <- metadata_df %>% dplyr::filter(.data[[mtd_name]] %in% metadata_combinations[[mtd_name]])
-    }
-
-    return(rownames(metadata_df))
-}
-
-remove_outlier_cells <- function(cell_names, umap_emb, percentile_threshold = 0.75) {
-    if (is.null(rownames(umap_emb))) {
-        rownames(umap_emb) <- seq_len(nrow(umap_emb))
-    }
-
-    if (length(intersect(cell_names, rownames(umap_emb))) == 0) {
-        stop("The cells were not found in the UMAP embedding")
-    }
-
-    umap_emb <- umap_emb[cell_names, ]
-
-    gmedian <- Gmedian::Gmedian(umap_emb)
-    distance_cells_gmedian <- (
-        (umap_emb[, 1] - gmedian[1, 1]) ^ 2 +
-        (umap_emb[, 2] - gmedian[1, 2]) ^ 2
-    ) ^ 0.5
-
-    threshold_distance <- quantile(distance_cells_gmedian, probs = percentile_threshold)
-
-    return(cell_names[distance_cells_gmedian <= threshold_distance])
-}
 
 subset_monocle_by_trajectory <- function(mon_obj, start_cells = NULL, end_cells = NULL) {
     if (is.null(start_cells) || is.null(end_cells)) {
@@ -175,13 +95,21 @@ custom_learn_graph <- function(mon_obj,
 
     learn_graph_controls$ncenter <- n_nodes
 
+    if (!("clusters" %in% names(mon_obj@clusters@listData$UMAP))) {
+        mon_obj <- monocle3::cluster_cells(mon_obj)
+    }
+
+    if (!is.null(metadata_for_nodes)) {
+        mon_obj@clusters@listData$UMAP$partitions <- setNames(factor(as.numeric(mon_obj@colData[[metadata_for_nodes]])), colnames(mon_obj))
+        levels(mon_obj@clusters@listData$UMAP$partitions) <- seq_len(nlevels(mon_obj@clusters@listData$UMAP$partitions))
+        mon_obj@clusters@listData$UMAP$clusters <- setNames(factor(as.numeric(mon_obj@colData[[metadata_for_nodes]])), colnames(mon_obj))
+        levels(mon_obj@clusters@listData$UMAP$clusters) <- seq_len(nlevels(mon_obj@clusters@listData$UMAP$clusters))
+    }
+
     if (use_partition) {
         learn_graph_controls$ncenter <- NULL
 
-        if (!is.null(metadata_for_nodes)) {
-            mon_obj@clusters@listData$UMAP$partitions <- setNames(factor(as.numeric(mon_obj@colData[[metadata_for_nodes]])), colnames(mon_obj))
-            levels(mon_obj@clusters@listData$UMAP$partitions) <- seq_len(nlevels(mon_obj@clusters@listData$UMAP$partitions))
-        }
+        
     }
 
     return(monocle3::learn_graph(
