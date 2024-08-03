f49cc7cdb6cf7594536ad0c7396698515eb23990
AndiMunteanu <munteanu_andi98@yahoo.com>
2024-08-03 14:20:31 +0300

    [feat] Add function that writes expression matrix to h5 as dense

--- R/hdf5_utils.R
+++ R/hdf5_utils.R
@@ -24,7 +24,7 @@ read_gene_from_dense_h5 <- function(gene_names,
     rownames(gene_matrix) <- gene_names
 
     if (add_rownames) {
-        colnames(gene_matrix) <- rhdf5::h5read(matrix_h5_path, "cells")
+        colnames(gene_matrix) <- as.character(rhdf5::h5read(matrix_h5_path, "cells"))
     }
 
     return(gene_matrix)
@@ -32,4 +32,75 @@ read_gene_from_dense_h5 <- function(gene_names,
 
 read_gene_from_sparse_h5 <- function() {
 
+}
+
+
+write_gene_matrix_dense_h5 <- function(expression_matrix,
+                                    file_path,
+                                    compression_level = 7,
+                                    chunk_size = 100,
+                                    all_at_once = TRUE,
+                                    garbage_thresh = 2) {
+
+    if (file.exists(file_path)) {
+        warning("Overwriting existing file. Waiting for 5 seconds...")
+        Sys.sleep(5.5)
+        file.remove(file_path)
+    }
+
+    if (is.null(rownames(expression_matrix))) {
+        rownames(expression_matrix) <- paste0("gene_", seq_len(nrow(expression_matrix)))
+    }
+
+    if (is.null(colnames(expression_matrix))) {
+        colnames(expression_matrix) <- paste0("cell_", seq_len(ncol(expression_matrix)))
+    }
+    rhdf5::h5createFile(file_path)
+    rhdf5::h5createDataset(
+        file_path,
+        "expression_matrix",
+        dims = dim(expression_matrix),
+        maxdims = dim(expression_matrix),
+        storage.mode = "double",
+        chunk = c(chunk_size, ncol(expression_matrix)),
+        level = compression_level
+    )
+
+    rhdf5::h5write(rownames(expression_matrix), file_path, "genes")
+    rhdf5::h5write(colnames(expression_matrix), file_path, "cells")
+
+    if (all_at_once) {
+        if (inherits(expression_matrix, "dgCMatrix")) {
+            expression_matrix <- as.matrix(expression_matrix)
+        }
+        rhdf5::h5write(expression_matrix, file_path, "expression_matrix")
+        return()
+    }
+
+    nchunks <- ceiling(nrow(expression_matrix) / chunk_size)
+    ngenes <- nrow(expression_matrix)
+
+    garbage_sum <- 0
+    for (i in seq_len(nchunks)) {
+        start <- (i - 1) * chunk_size + 1
+        end <- min(i * chunk_size, ngenes)
+        written_mat <- expression_matrix[start:end, ]
+        if (garbage_thresh > 0) garbage_sum <- garbage_sum + object.size(written_mat) / 1024^3
+        if (inherits(written_mat, "dgCMatrix")) {
+            written_mat <- as.matrix(written_mat)
+            if (garbage_thresh > 0) garbage_sum <- garbage_sum + object.size(written_mat) / 1024^3
+        }
+        if (garbage_thresh > 0 && garbage_sum > garbage_thresh) {
+            gc()
+            garbage_sum <- 0
+        }
+
+        rhdf5::h5write(
+            obj = written_mat,
+            file = file_path,
+            name = "expression_matrix",
+            index = list(start:end, NULL)
+        )
+    }
+    rhdf5::h5closeAll()
 }
