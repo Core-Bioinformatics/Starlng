577075a14b4bcf48b37949bbc94e6011a2652c42
AndiMunteanu <munteanu_andi98@yahoo.com>
2026-06-22 13:01:00 +0300

    [chore] Remove the paper-related scripts from this repository. Please consult https://github.com/Core-Bioinformatics/Starlng_paper

--- paper/comparison_with_scenic/3_scenic_step1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pyscenic grn \
-    --num_workers 10 \
-    -o expr_mat.adjacencies.tsv \
-    seurat_info.loom \
-    allTFs_hg38.txt
--- paper/comparison_with_scenic/5_scenic_step3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pyscenic aucell \
-    seurat_info.loom \
-    reg.csv \
-    --output pyscenic_output.loom \
-    --num_workers 10
